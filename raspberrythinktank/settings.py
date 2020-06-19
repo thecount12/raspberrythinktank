5ee81db6fb5b010261edc9a5a513d6bd299c7688
William Gunnells <gunnells@gmail.com>
2020-06-19 15:18:58 -0700

    white noise

--- raspberrythinktank/settings.py
+++ raspberrythinktank/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ]
 
 ROOT_URLCONF = 'raspberrythinktank.urls'
