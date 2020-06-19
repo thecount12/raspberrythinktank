9bb6cad638d1d79adb2025f9e9020f3908872d60
William Gunnells <gunnells@gmail.com>
2020-06-19 15:39:04 -0700

    white noise problem fixed syntax

--- raspberrythinktank/settings.py
+++ raspberrythinktank/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'raspberrythinktank.urls'
